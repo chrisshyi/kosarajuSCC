47228e3b49331112c2b8bbea59aa43523b97dcf1
Chris Shyi <chrisshyi13@gmail.com>
2018-05-26 14:24:38 -0400

    Test implementation
    Recursive implementation works on test cases, but overflows the stack when
    used on the actual input. Need to construct iterative version.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,23 @@ use std::fs::File;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io::{BufReader, BufRead};
+use std::cell::RefCell;
+
+
+pub fn compute_scc(file_path: &str) -> RefCell<HashMap<i32, i32>> {
+
+    let mut file = File::open(file_path).unwrap();
+
+    let (reverse_graph, num_vertices) = make_reverse_graph(file);
+
+    let finishing_times = dfs_outer(reverse_graph, true, num_vertices);
+
+    file = File::open(file_path).unwrap();
+    let relabelled_graph = relabel_graph(file, &*finishing_times.borrow());
+
+    let scc_map = dfs_outer(relabelled_graph, false, num_vertices);
+    scc_map
+}
 
 /// Creates the adjacency list representation of a directed acyclic graph, but
 /// with all the edges reversed
@@ -11,7 +28,7 @@ use std::io::{BufReader, BufRead};
 /// 
 /// example:
 /// 2 5 indicates there an edge from vertex 2 to vertex 5
-pub fn make_reverse_graph(file: File) -> (HashMap<i32, HashSet<i32>>, i32) {   
+fn make_reverse_graph(file: File) -> (HashMap<i32, HashSet<i32>>, i32) {   
     let mut graph: HashMap<i32, HashSet<i32>> = HashMap::new();
     let reader = BufReader::new(file);
     let (mut tail_vertx, mut head_vertx) = (0, 0); // Compiler won't allow use of uninitialized variables
@@ -48,19 +65,24 @@ pub fn make_reverse_graph(file: File) -> (HashMap<i32, HashSet<i32>>, i32) {
 /// Returns:
 ///     A mapping of vertices to their finishing times (first pass), or a mapping of leader variables 
 ///     to their SCC sizes
-pub fn dfs_outer(graph: HashMap<i32, HashSet<i32>>, first_pass: bool, num_vertices: i32) -> HashMap<i32, i32> {
-    let mut explored_nodes: HashSet<i32> = HashSet::new();
-    let mut result_map: HashMap<i32, i32> = HashMap::new();
+fn dfs_outer(graph: HashMap<i32, HashSet<i32>>, first_pass: bool, num_vertices: i32) -> RefCell<HashMap<i32, i32>> {
+    let mut explored_nodes: RefCell<HashSet<i32>> = RefCell::new(HashSet::new());
+    let mut result_map: RefCell<HashMap<i32, i32>> = RefCell::new(HashMap::new());
 
-    let mut finishing_time = Box::new(1);
-    let mut leader_val = Box::new(0);
+    let finishing_time = RefCell::new(0);
+    let leader_val = RefCell::new(0);
     for vertex in (1..num_vertices + 1).rev() {
-        if !explored_nodes.contains(&vertex) {
+        if first_pass {
+            println!("First pass, {} vertices left", vertex);
+        } else {
+            println!("Second pass, {} vertices left", vertex);
+        }
+        if !explored_nodes.borrow_mut().contains(&vertex) {
             if first_pass {
-                dfs_inner(&graph, vertex, first_pass, &mut finishing_time, &mut result_map, &mut explored_nodes);
+                dfs_inner(&graph, vertex, first_pass, &finishing_time, &result_map, &explored_nodes);
             } else {
-                *leader_val = vertex;
-                dfs_inner(&graph, vertex, first_pass, &mut leader_val, &mut result_map, &mut explored_nodes);
+                *leader_val.borrow_mut() = vertex;
+                dfs_inner(&graph, vertex, first_pass, &leader_val, &result_map, &explored_nodes);
             }
         }  
     }
@@ -80,33 +102,37 @@ pub fn dfs_outer(graph: HashMap<i32, HashSet<i32>>, first_pass: bool, num_vertic
 ///     leader_or_ft_map: if first pass, maps vertices to their finishing times, else, maps each leader to the size of their SCCs
 ///     explored_nodes: set of explored nodes
 fn dfs_inner(graph: &HashMap<i32, HashSet<i32>>, start_vertex: i32, first_pass: bool, 
-leader_or_ftime: &mut Box<i32>, leader_or_ft_map: &mut HashMap<i32, i32>, explored_nodes: &mut HashSet<i32>) {
-    let mut stack: Vec<i32> = Vec::new();
+leader_or_ftime: &RefCell<i32>, leader_or_ft_map: &RefCell<HashMap<i32, i32>>, explored_nodes: &RefCell<HashSet<i32>>) {
 
-    stack.push(start_vertex);
+    // introduce a block here so that the mutable borrow will be dropped at the end of the block
+    {
+        explored_nodes.borrow_mut().insert(start_vertex);
+    }
 
-    while !stack.is_empty() {
-        let cur_vertx = stack.pop().unwrap();
-        explored_nodes.insert(cur_vertx);
-
-        for neighbor in graph.get(&cur_vertx).unwrap().iter() {
-            if !explored_nodes.contains(neighbor) {
-                stack.push(*neighbor);
+    if graph.contains_key(&start_vertex) {
+        for neighbor in graph.get(&start_vertex).unwrap().iter() {
+            if !explored_nodes.borrow().contains(neighbor) {
+                dfs_inner(graph, *neighbor, first_pass, leader_or_ftime, leader_or_ft_map, explored_nodes);
             }
         }
-        if first_pass {
-            **leader_or_ftime += 1;
-            leader_or_ft_map.insert(cur_vertx, **leader_or_ftime);
-        } else {
-            let scc_size = leader_or_ft_map.entry(**leader_or_ftime).or_insert(0);
-            *scc_size += 1;
-        }
-    }                        
+    } else {
+        return;
+    }
+
+    if first_pass {
+        *leader_or_ftime.borrow_mut() += 1;
+        // println!("Finishing time for {} is {}", start_vertex, *leader_or_ftime.borrow());
+        leader_or_ft_map.borrow_mut().insert(start_vertex, *leader_or_ftime.borrow());
+    } else {
+        let mut borrowed_map = (*leader_or_ft_map).borrow_mut();
+        let scc_size = borrowed_map.entry(*leader_or_ftime.borrow()).or_insert(0);
+        *scc_size += 1;
+    }
 }
 
 /// Relabels the vertices of a graph using their finishing times computed by 
 /// the first pass DFS of Kosaraju's algorithm
-pub fn relabel_graph(file: File, finishing_times: HashMap<i32, i32>) -> HashMap<i32, HashSet<i32>> {
+fn relabel_graph(file: File, finishing_times: &HashMap<i32, i32>) -> HashMap<i32, HashSet<i32>> {
     let mut graph: HashMap<i32, HashSet<i32>> = HashMap::new();
     let reader = BufReader::new(file);
     let (mut tail_vertx, mut head_vertx) = (0, 0); // Compiler won't allow use of uninitialized variables
@@ -141,5 +167,26 @@ mod tests {
         assert!(reverse_graph.get(&2).unwrap().contains(&1));
         assert!(reverse_graph.get(&1).unwrap().contains(&3));
         assert!(reverse_graph.get(&3).unwrap().contains(&2));
+    }
+
+    #[test]
+    fn test_input_file2() {
+        let scc_map = compute_scc("/home/chris/Workspace/rust/kosarajuSCC/test_files/test2.txt");
+        assert!(!scc_map.borrow().is_empty());
+
+        assert_eq!(*(scc_map.borrow().get(&6).unwrap()), 3);
+        assert_eq!(*(scc_map.borrow().get(&7).unwrap()), 1);
+        assert_eq!(*(scc_map.borrow().get(&4).unwrap()), 3);
+    }
+
+
+    #[test]
+    fn test_input_file3() {
+        let scc_map = compute_scc("/home/chris/Workspace/rust/kosarajuSCC/test_files/test3.txt");
+        assert!(!scc_map.borrow().is_empty());
+
+        assert_eq!(*(scc_map.borrow().get(&6).unwrap()), 4);
+        assert_eq!(*(scc_map.borrow().get(&2).unwrap()), 1);
+        assert_eq!(*(scc_map.borrow().get(&1).unwrap()), 1);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 extern crate kosarajuSCC;
 use std::fs::File;
 
-use kosarajuSCC::{make_reverse_graph, dfs_outer, relabel_graph};
+use kosarajuSCC::{compute_scc};
 
 fn main() {
-    println!("Hello, world!");
-    let mut file = File::open("/home/chris/Workspace/rust/kosarajuSCC/SCC.txt").unwrap();
-    let (reverse_graph, num_vertices) = make_reverse_graph(file);
+    let scc_map = compute_scc("/home/chris/Workspace/rust/kosarajuSCC/SCC.txt");
 
-    let finishing_times = dfs_outer(reverse_graph, true, num_vertices);
+    let borrowed_map = scc_map.borrow();
 
-    file = File::open("/home/chris/Workspace/rust/kosarajuSCC/SCC.txt").unwrap();
-    let relabelled_graph = relabel_graph(file, finishing_times);
-
-    let scc_map = dfs_outer(relabelled_graph, false, num_vertices);
-
-    for key in scc_map.keys() {
-        println!("SCC {} has size {}", key, scc_map.get(key).unwrap());
+    for key in borrowed_map.keys() {
+        println!("SCC {} has size {}", key, borrowed_map.get(key).unwrap());
     }
 }
